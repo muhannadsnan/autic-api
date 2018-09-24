60329f978ab3e6d9c2166bb8b73770ab11d81d09
Muhannad Senan <msn-23@live.com>
2018-09-24 15:07:24 +0200

    EDITED: environment db port number

--- environment/environment.js
+++ environment/environment.js
@@ -2,11 +2,11 @@ module.exports = {
     db: {
       uri: 'mongodb://auticdb:1jJc6p88mYNmLbJt0XWEOYT1L0GDc6L6MVr5Gy7zyDekZeMZVeVSPLAfM99vEurRH3XPX1nKAnqDfsvvGGTcMw==@auticdb.documents.azure.com:10255/?ssl=true',
       messageConnected: "Connected to Azure DB!",
-      port: 3000
+      port: process.env.PORT ||3000
     },
     db2: {
         uri: 'mongodb://localhost/autic',
         messageConnected: "Connected to local DB!",
-        port: 3000
+        port: 3000 || process.env.PORT
     },
 }; 

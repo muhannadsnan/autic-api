363a446312623f70357fa30595cb8efaa18ae807
muhannadsnan <msn-23@live.com>
2018-09-27 09:32:16 +0200

    cleaning code

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-// configure the app to use bodyParser()
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // configure the app to use bodyParser()
+//ENV
 var environment = require('./environment/environment')
 global.ENV = environment.env1;
+//CONTROLLERS
 var catsController = require("./controllers/catsController");
-
 // ROUTES
 var routing = require('./routes');
 app.use(routing.cats);
@@ -14,7 +14,5 @@ app.get('/autic', catsController.index);
 app.get('/', (req, resp)=>{
     resp.send(`<h1>${ENV.db.messageConnected} on port:<br> ${ENV.port}</h1>`);
 });
-
-// LISTENS
-app.listen(ENV.port);
-console.log(`Node running on port ${ENV.port}...`);
+// LISTEN
+app.listen(ENV.port, ()=> console.log(`Node running on port ${ENV.port}...`) );

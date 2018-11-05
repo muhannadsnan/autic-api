224a1fa261f86f05e16ad601c2ebf68c20fa6d72
muhannadsnan <msn-23@live.com>
2018-11-05 09:25:50 +0100

    add lastUpdated to the pushed data

--- controllers/devsController.js
+++ controllers/devsController.js
@@ -13,7 +13,10 @@ exports.insertDataFromDevice = function (req, resp) {
     IoT.getTwin(req.params.devID, function(err, twin){ 
         if (err) throw err;
         var query = {deviceId: req.params.devID};
-        var newData = {"deviceId":twin.deviceId, "timestamp": twin.lastActivityTime, ...twin.properties.reported.tags};
+        var newData = {"deviceId":twin.deviceId, 
+                        "lastUpdated": twin.properties.desired.$metadata.$lastUpdated,
+                        "version": twin.properties.desired.$metadata.$lastUpdatedVersion,
+                        ...twin.properties.reported.tags};
         Device.createDataForDevice(query, newData, function(err, result){
             if(err) throw err;
             resp.json(newData);

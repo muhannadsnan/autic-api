2c7bd452f3326a024290403bbff9b00ee39b9e7f
muhannadsnan <msn-23@live.com>
2018-12-13 13:07:28 +0100

    edit port

--- environment/environment.js
+++ environment/environment.js
@@ -1,7 +1,7 @@
 module.exports = {
     env1: {
-        port: 3000, //app port on azure db host
-        // port: process.env.PORT, //app port on azure db host
+        // port: 3000, //app port on azure db host
+        port: process.env.PORT, //app port on azure db host
         db: {
             uri: 'mongodb://auticdb:1jJc6p88mYNmLbJt0XWEOYT1L0GDc6L6MVr5Gy7zyDekZeMZVeVSPLAfM99vEurRH3XPX1nKAnqDfsvvGGTcMw==@auticdb.documents.azure.com:10255/?ssl=true',
             messageConnected: "Connected to Azure DB!"
